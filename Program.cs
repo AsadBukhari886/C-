8b9164f516b63773b93e6d07941171a6005fd71f
Ahadbukhari <asadbukhari886@gmail.com>
2021-03-16 12:02:48 +0500

    adding program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("this is asad bukhari and i ma the best frontend code editor in the world");
+        }
+    }
+}
